bd87a13792c185be878aa664a415abb1339d3bf2
Aline Alves Martinez <alinemartinez@brq.com>
2018-09-28 19:53:31 -0300

    git init

---
+++ calculadora/src/main/java/com/aline/calculadora/App.java
@@ -0,0 +1,13 @@
+package com.aline.calculadora;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
